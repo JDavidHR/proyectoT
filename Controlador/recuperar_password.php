36d3e9bf25226298551900aae642386253b67fd9
Natalia AV <nagudelo@iegabo.edu.co>
2021-08-13 17:32:12 -0500

    recuperar clave implementado

---
+++ Controlador/recuperar_password.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" id="bootstrap-css">
+  <link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+    <div class="col-lg-offset-3 col-lg-6">
+    <?php
+    //condicion donde se rectifica que los campos no esten vacios y que esten definidos
+    if(!empty($_POST['email']) && !empty($_POST['tipousuario']) && !empty($_POST['pass'])){
+
+            require_once '../Modelo/MySQL.php';//se llama la pagina mysql.php para hacer la respectiva conexion con la BD
+            //declaracion de las variables donde se almacenan los datos de los respectivos campos llenados del formulario metodo post
+            $usuario = $_POST["email"];
+            $tipousuario = $_POST['tipousuario'];
+            $password = $_POST['pass'];
+
+            
+            $mysql = new MySQL;//nuevo mysql
+            $mysql->conectar();//funcion almacenada en mysql.php
+            //consulta de la insercion de datos en la base de datos, donde hace las respectivas consultas
+            if($tipousuario == 1){
+                echo " estudiante: " . $tipousuario;
+                echo " correo: " . $usuario;
+                $usuarios= $mysql->efectuarConsulta("select asistencia.estudiante.id_estudiante, asistencia.estudiante.nombres, asistencia.estudiante.tipo_usuario_id_tipo_usuario, asistencia.estudiante.correo from estudiante where correo = '".$usuario."' and estado = 1"); 
+                            //Cuento si la consulta esta vacia
+                            if (!empty($usuarios)){
+                                //consulto si existen filas en el objeto
+                                if(mysqli_num_rows($usuarios) > 0){
+                                    //inicio la session
+                                    session_start();
+                                    //recorro el resultado de la consulta y la almaceno en una variable
+                                    while ($resultado= mysqli_fetch_assoc($usuarios)){
+                                        //almaceno los resultados en variables
+                                        $id_estudiante = $resultado["id_estudiante"];
+                                        $nombre = $resultado["nombres"];
+                                        $tipo_usuario = $resultado['tipo_usuario_id_tipo_usuario'];
+                                        $correo = $resultado['correo'];
+                                    }
+                                    //$_SESSION['tipousuario'] = $tipo_usuario;
+                                    $_SESSION['idEstudiante'] = $id_estudiante;
+                                    $_SESSION['correo'] = $correo;
+
+                                    $sql=$mysql->efectuarConsulta("UPDATE asistencia.estudiante SET clave ='".$password."' WHERE correo = '".$usuario."'");
+                                    if($sql){
+                                    //mensaje de salida (alert) cuanod la consulta es exitosa con su respectiva redireccion de pagina
+                                    echo"<script type=\"text/javascript\">alert('Se actualizo correctamente'); window.location='../login.php';</script>";
+                                    }else{
+                                    //mensaje de salida en caso de que la consulta falle
+                                    echo"<script type=\"text/javascript\">alert('Se produjo un error'); window.location='../login.php';</script>";
+                                    }
+                                }else{
+                                    //Mesanje de error por si no hay filas en la consulta
+                                    echo "<div class=\"alert alert-warning alert-dismissible\"><a href=\"../login.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Alerta!</strong>Usuario o contraseña incorrecta o esta inactivo.</div>";
+                                    header( "refresh:3;url=../login.php" );
+                                }                                    
+                            }else{
+                                //Mensaje de error si la consulta esta vacia
+                                echo "<div class=\"alert alert-warning alert-dismissible\"><a href=\"../login.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Alerta!</strong>Usuario o contraseña incorrecta o esta inactivo.</div>";
+                                header( "refresh:3;url=../login.php" ); 
+                            } 
+                        
+            }elseif($tipousuario == 2){ 
+                echo " docente: " . $tipousuario;
+
+                $usuarios= $mysql->efectuarConsulta("select asistencia.docente.id_docente, asistencia.docente.nombres, asistencia.docente.tipo_usuario_id_tipo_usuario, asistencia.docente.correo from docente where correo = '".$usuario."' and estado = 1"); 
+                            //Cuento si la consulta esta vacia
+                            if (!empty($usuarios)){
+                                //consulto si existen filas en el objeto
+                                if(mysqli_num_rows($usuarios) > 0){
+                                    //inicio la session
+                                    session_start();
+                                    //recorro el resultado de la consulta y la almaceno en una variable
+                                    while ($resultado= mysqli_fetch_assoc($usuarios)){
+                                        //almaceno los resultados en variables
+                                        $id_docente = $resultado["id_docente"];
+                                        $nombre = $resultado["nombres"];
+                                        $tipo_usuario = $resultado['tipo_usuario_id_tipo_usuario'];
+                                        $correo = $resultado['correo'];
+                                    }
+                                    //$_SESSION['tipousuario'] = $tipo_usuario;
+                                    $_SESSION['idDocente'] = $id_docente;
+                                    $_SESSION['correo'] = $correo;
+
+                                    $sql=$mysql->efectuarConsulta("UPDATE asistencia.docente SET clave ='".$password."' WHERE correo = '".$usuario."'");
+                                    if($sql){
+                                    //mensaje de salida (alert) cuanod la consulta es exitosa con su respectiva redireccion de pagina
+                                    echo"<script type=\"text/javascript\">alert('Se actualizo correctamente'); window.location='../login.php';</script>";
+                                    }else{
+                                    //mensaje de salida en caso de que la consulta falle
+                                    echo"<script type=\"text/javascript\">alert('Se produjo un error'); window.location='../login.php';</script>";
+                                    }
+                                }else{
+                                    //Mesanje de error por si no hay filas en la consulta
+                                    echo "<div class=\"alert alert-warning alert-dismissible\"><a href=\"../login.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Alerta!</strong>Usuario o contraseña incorrecta o esta inactivo.</div>";
+                                    header( "refresh:3;url=../login.php" );
+                                }                                    
+                            }else{
+                                //Mensaje de error si la consulta esta vacia
+                                echo "<div class=\"alert alert-warning alert-dismissible\"><a href=\"../login.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Alerta!</strong>Usuario o contraseña incorrecta o esta inactivo.</div>";
+                                header( "refresh:3;url=../login.php" ); 
+                            } 
+
+            }elseif($tipousuario == 3){
+                echo " administrador: " . $tipousuario;
+
+                $usuarios= $mysql->efectuarConsulta("select asistencia.administrador.id_administrador, asistencia.administrador.nombres, asistencia.administrador.tipo_usuario_id_tipo_usuario, asistencia.administrador.correo from administrador where correo = '".$usuario."' and estado = 1"); 
+                            //Cuento si la consulta esta vacia
+                            if (!empty($usuarios)){
+                                //consulto si existen filas en el objeto
+                                if(mysqli_num_rows($usuarios) > 0){
+                                    //inicio la session
+                                    session_start();
+                                    //recorro el resultado de la consulta y la almaceno en una variable
+                                    while ($resultado= mysqli_fetch_assoc($usuarios)){
+                                        //almaceno los resultados en variables
+                                        $id_administrador = $resultado["id_administrador"];
+                                        $nombre = $resultado["nombres"];
+                                        $tipo_usuario = $resultado['tipo_usuario_id_tipo_usuario'];
+                                        $correo = $resultado['correo'];
+                                    }
+                                    //$_SESSION['tipousuario'] = $tipo_usuario;
+                                    $_SESSION['idAdministrador'] = $id_administrador;
+                                    $_SESSION['correo'] = $correo;
+
+                                    $sql=$mysql->efectuarConsulta("UPDATE asistencia.administrador SET clave ='".$password."' WHERE correo = '".$usuario."'");
+                                    if($sql){
+                                    //mensaje de salida (alert) cuanod la consulta es exitosa con su respectiva redireccion de pagina
+                                    echo"<script type=\"text/javascript\">alert('Se actualizo correctamente'); window.location='../login.php';</script>";
+                                    }else{
+                                    //mensaje de salida en caso de que la consulta falle
+                                    echo"<script type=\"text/javascript\">alert('Se produjo un error'); window.location='../login.php';</script>";
+                                    }
+                                }else{
+                                    //Mesanje de error por si no hay filas en la consulta
+                                    echo "<div class=\"alert alert-warning alert-dismissible\"><a href=\"../login.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Alerta!</strong>Usuario o contraseña incorrecta o esta inactivo.</div>";
+                                    header( "refresh:3;url=../login.php" );
+                                }                                    
+                            }else{
+                                //Mensaje de error si la consulta esta vacia
+                                echo "<div class=\"alert alert-warning alert-dismissible\"><a href=\"../login.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Alerta!</strong>Usuario o contraseña incorrecta o esta inactivo.</div>";
+                                header( "refresh:3;url=../login.php" ); 
+                            } 
+            }
+            //Desconecto la conexion de la bD
+            $mysql->desconectar();          
+        }else{
+            echo "<div class=\"alert alert-warning alert-dismissible\"><a href=\"../login.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Alerta!</strong>No se enviaron los datos.</div>";
+            header( "refresh:3;url=../login.php" );
+        }
+
+    ?>
+    </div>
+    <!-- Llamado de los respectivos scripts -->
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
--- paginas/recuperar_clave.php
+++ paginas/recuperar_clave.php
@@ -4,17 +4,19 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!--<meta http-equiv="X-UA-Compatible" content="ie=edge">-->
-  <title>Bienvenido al sistema de login</title>
-  <link rel="stylesheet" href="assets/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="assets/css/login.css">
+  <title>Recuperar clave</title>
+  <link rel="stylesheet" href="../assets/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../assets/css/login.css">
 </head>
 <body>
   <?php 
   session_start();
-  if(!isset($_SESSION['tipousuario'])){
+  if(isset($_SESSION['tipousuario'])){
+    header( "refresh:0;url=../index.php" );    
+  }else{
   //llamado del archivo mysql
-  require_once 'Modelo/MySQL.php';
+  require_once '../Modelo/MySQL.php';
   //creacion de nueva "consulta"
   $mysql = new MySQL;
   //se conecta a la base de datos
@@ -30,22 +32,23 @@
       <div class="card login-card">
         <div class="row no-gutters">
           <div class="col-md-7">
-            <img src="assets/images/login.png" alt="login" class="login-card-img">
+            <img src="../assets/images/login.png" alt="login" class="login-card-img">
           </div>
           <div class="col-md-5">
             <div class="card-body">
               <p class="login-card-description">Bienvenid@, por favor ingrese los datos correspondientes</p>
-              <form action="Controlador/login_validacion.php" method="POST">
+              <label>Recuerde no compartir los datos con nadie.</label><br><br>
+              <form action="../Controlador/recuperar_password.php" method="POST">
                   <div class="form-group">
-                    <label>Documento</label>
-                    <input type="text" name="doc" id="doc" class="form-control" placeholder="No. de Documento" required="">
+                    <label>Correo</label>
+                    <input type="email" name="email" id="email" class="form-control" placeholder="Correo electr&oacute;nico" required="">
                   </div>
                   <div class="form-group ">
-                    <label>Clave</label>                
+                    <label>Nueva clave</label>                
                     <input type="password" name="pass" id="pass" class="form-control" placeholder="***********" required="">
                   </div>
                   <div class="form-group mb-4">
-                      <select class="form-control " name="tipousuario" required>
+                      <select class="form-control" name="tipousuario" required>
                         <?php 
                         //ciclo while que nos sirve para traer cuales son los tipos de usuario (paciente, medico)
                           while ($resultado= mysqli_fetch_assoc($seleccionUsuario)){                         
@@ -57,11 +60,12 @@
                         ?>
                       </select>
                   </div>
-                  <input name="sesion" id="sesion" class="btn btn-block login-btn mb-4" type="submit" value="Iniciar sesi&oacute;n">
+                  <center>
+                    <input name="enviar" class="btn login-btn col-5" type="submit" value="Recuperar">
+                    <a href="../login.php"><input class="btn login-btn col-5" style="background: #D33E3E" value="Cancelar"></a>
+                  </center>
+                  
                 </form>
-
-                <a href="paginas/recuperar_clave.php" class="forgot-password-link">¿Olvidaste tu clave o no tienes una a&uacute;n?</a>
-                <br><br>
 
                 <nav class="login-card-footer-nav">
                   <div class="d-flex align-items-center justify-content-between small">
@@ -76,12 +80,10 @@
   </main>
 
   <?php
-    }else{
-         header( "refresh:0;url=index.php" );    
     }
   ?>
-  <script src="js/jquery-3.4.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
+  <script src="../js/jquery-3.4.1.min.js"></script>
+  <script src="../js/popper.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
 </body>
 </html>
